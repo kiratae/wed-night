30f580061562c3b28b269c4062dafda29e57453b
tanaphon.k@pc <tanaphon.k@outlook.com>
2023-05-11 22:15:29 +0700

    add button anim and control

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import "../app.css";
+    import IndexNavbar from '../lib/IndexNavbar.svelte';
+  </script>
+  
+  <IndexNavbar />
+  <div class="container mx-auto">
+    <slot />
+  </div>
